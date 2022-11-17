4c1f80750f68570a1821b3d8578f84751f743081
Jacek <jacek.smoliniec@gmail.com>
2022-11-17 22:28:40 +0100

    Add code that opens onet.pl website

--- src/admiinPackageName/Main.java
+++ src/admiinPackageName/Main.java
@@ -9,16 +9,23 @@ public class Main {
     WebDriver driver;
 
     public void invokeBrowser() {
-        System.setProperty("webdriver.chrome.driver", "\"C:\\Users\\Jacek\\Downloads\\chromedriver.exe\"");
-        driver = new ChromeDriver();
-        driver.manage().deleteAllCookies();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        try {
+            System.setProperty("webdriver.chrome.driver", "C:\\Users\\Jacek\\Documents\\IT\\java\\chrome_webdriver\\chromedriver.exe");
+            driver = new ChromeDriver();
+            driver.manage().deleteAllCookies();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+            driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 
+            driver.get("http://www.onet.pl");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     };
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    Main myObj = new Main();
+    myObj.invokeBrowser();
     }
 }
